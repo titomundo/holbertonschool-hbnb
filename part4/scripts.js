3bdf07982861002446669b33601b30d9859e3d0c
titomundo <tito.alberto.mundo@gmail.com>
2026-07-17 22:10:30 -0600

    add login, fetch and display list of places

--- part4/scripts.js
+++ part4/scripts.js
@@ -16,8 +16,71 @@ async function loginUser(email, password) {
   }
 }
 
+function checkAuthentication() {
+  const token = getCookie("token");
+  const loginLink = document.getElementById("login-link");
+
+  if (!token) {
+    loginLink.style.display = "block";
+  } else {
+    loginLink.style.display = "none";
+    fetchPlaces(token);
+  }
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+}
+
+async function fetchPlaces(token) {
+  const response = await fetch("http://127.0.0.1:5000/api/v1/places/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    displayPlaces(data);
+  }
+}
+
+function displayPlaces(places) {
+  list = document.getElementById("places-list").children[0];
+  list.textContent = "";
+
+  places.forEach((place) => {
+    const li = document.createElement("li");
+    const card = document.createElement("div");
+    const title = document.createElement("h3");
+    const description = document.createElement("p");
+    const link = document.createElement("a");
+
+    card.classList.add("card", "shadow");
+    title.textContent = place.title;
+    description.textContent = `Price per night: $${place.price}`;
+    link.textContent = "See details";
+    link.href = `http://127.0.0.1:5000/api/v1/places/${place.id}`;
+
+    card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(link);
+    li.appendChild(card);
+    list.appendChild(li);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("login-form");
+  const token = getCookie("token");
 
   if (loginForm) {
     loginForm.addEventListener("submit", async (e) => {
@@ -28,4 +91,6 @@ document.addEventListener("DOMContentLoaded", () => {
       loginUser(email, password);
     });
   }
+
+  checkAuthentication();
 });
